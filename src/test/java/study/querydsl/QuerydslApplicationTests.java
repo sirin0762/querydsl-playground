9cfcc10b2a93850654cfd347c5555925f7ca9ded
sirin0762 <tlsdydwls123@gmail.com>
2022-01-26 21:23:10 +0900

    CHORE : 초기 세팅

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,41 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+            .selectFrom(qHello)
+            .fetchOne();
+
+        assertThat(result, is(hello));
+        assertThat(result.getId(), is(hello.getId()));
+    }
+
+}
