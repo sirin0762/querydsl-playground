2fb9169a7ad830d06ec155b62c7e6d307ba0fbd4
sirin0762 <tlsdydwls123@gmail.com>
2022-01-29 21:07:12 +0900

    TEST : case, 상수값, 문자열 합치기

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,8 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -346,4 +348,60 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void basicCase() {
+        List<String> result = queryFactory
+            .select(member.age
+                .when(10).then("열살")
+                .when(20).then("스무살")
+                .otherwise("기타"))
+            .from(member)
+            .fetch();
+
+        for(String s : result) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void complexCase() {
+        List<String> result = queryFactory
+            .select(
+                new CaseBuilder()
+                    .when(member.age.between(0, 20)).then("0 ~ 20살")
+                    .when(member.age.between(21, 30)).then("21살 ~ 30살")
+                    .otherwise("기타")
+            )
+            .from(member)
+            .fetch();
+
+        for(String s : result) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory
+            .select(member.username, Expressions.constant("A"))
+            .from(member)
+            .fetch();
+
+        for(Tuple tuple : result) {
+            System.out.println(tuple);
+        }
+    }
+
+    @Test
+    public void concat() {
+        List<String> result = queryFactory
+            .select(member.username.concat("_").concat(member.age.stringValue()))
+            .from(member)
+            .where(member.username.eq("Member1"))
+            .fetch();
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
 }
